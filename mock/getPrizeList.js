3fbcb9368300fe13ed9857294f551abd605411e3
Junjun Chen <2548077705@qq.com>
2025-11-18 19:21:46 +0800

    :star: 添加了必要的jsDoc注释类型

--- mock/getPrizeList.js
+++ mock/getPrizeList.js
@@ -1,3 +1,13 @@
+/**
+ * @typedef {Object} Prize
+ * @property {string} [name]
+ * @property {string} [email]
+ */
+/**
+ * Mock 产生基本奖品列表数据接口
+ * @returns {Promise<Array<Prize>>}
+ */
+
 function getPrizeList() {
     const prizeList = Array(5).fill().map((_, i) => ({
         name: 'test' + i,
